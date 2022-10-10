b7815f01941be54e808b955e9c2ab926e0b2776a
Saidur Rahman <saidurrahman1473@gmail.com>
2022-10-11 00:47:13 +0600

    navbar errorFix

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,10 +10,7 @@ const Home = () => {
     return (
         <div className='grid grid-cols-2' style={{margin:'0px 5rem'}}>
 
-          <div className="">
-            <h1 className='text-6xl mt-32 font-bold  leading tracking-wide opacity-82 mb-20'>The Smarter <br /> Way to Learn is<br />  <span className=' text-6xl text-orange-400'> Practicing QUIZ.</span></h1>
-
-         </div>
+          
            
         
           
