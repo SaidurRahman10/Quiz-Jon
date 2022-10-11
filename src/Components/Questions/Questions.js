ebe5c44452bd2fac2dccca9f7193a407ab07561d
Saidur Rahman <saidurrahman1473@gmail.com>
2022-10-12 00:21:03 +0600

    fix some error

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -7,10 +7,10 @@ const Questions = ({questions}) => {
 
     const {question,correctAnswer,options} = questions;
     
-    const notify = () => {
-        toast.success(`Answer is: ${correctAnswer}`, {
+    const watchAns = () => {
+        toast.success(`Answer is ( ${correctAnswer} )`, {
             position: "top-center",
-            autoClose: 5000,
+            autoClose: 3000,
             draggable: false
         });
     }
@@ -19,13 +19,13 @@ const Questions = ({questions}) => {
         if (id === correctAnswer) {
             toast.success(" Your Answer is Correct!", {
                 position: "top-center",
-                autoClose: 2000,
+                autoClose: 2500,
                 draggable: false
             });
         } else {
             toast.error("Your Answer is Wrong", {
                 position: "top-center",
-                autoClose: 2000,
+                autoClose: 2500,
                 draggable: false,
 
             });
@@ -40,16 +40,16 @@ const Questions = ({questions}) => {
         <div className='text-center flex flex-col justify-center items-center shadow-lg p-10 pb-10 rounded-3xl '>
             
             <h1 className='text-2xl  w-1/2 my-10 font-bold text-orange-600'>Quiz:  {question}</h1>
-            <button type="button" onClick={notify} className="px-8 py-3 font-semibold rounded-full dark:bg-gray-100 dark:text-gray-800"><EyeIcon className="h-6 w-6 text-blue-500"/></button>
+            <button type="button" onClick={watchAns} className="absolute -mt-44 px-8 py-3 ml-[800px]  bg-slate-500 font-semibold rounded-full "><EyeIcon className="h-6 w-6 text-white"/></button>
 
             <div className=" grid grid-cols-2 gap-5 ">
-                {/* <button className='p-10 border-orange-300 border rounded-2xl font-bold'>{options[0]} </button> */}
+            
 
                 {
                     options.map(option => <section key={option}>
 
                      <div className=''>
-                            <button type="button" id='button' onClick={() => { rightAnswer(option) }} className="p-10 border-orange-300 border rounded-2xl font-bold">{option}</button>
+                            <button type="button" id='button' onClick={() => { rightAnswer(option) }} className="px-10 py-5 border-orange-300 border rounded-2xl font-bold">{option}</button>
                             <ToastContainer
                             />
                         </div>
